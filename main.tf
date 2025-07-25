dcae942c2b5055ec8684b7b9211f48c2a132cd96
SK963 <kumarshubham963852741@gmail.com>
2025-07-25 15:35:03 +0530

    code commit

---
+++ main.tf
@@ -0,0 +1,131 @@
+# variables.tf
+variable "aws_region" {
+  default = "ap-south-1"
+}
+
+variable "container_image" {
+  description = "sk963/sample-flask-app:latest"
+  type        = string
+}
+
+
+# resources.tf
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "subnet" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = data.aws_availability_zones.available.names[0]
+}
+
+data "aws_availability_zones" "available" {}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route_table" "rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+resource "aws_route_table_association" "rta" {
+  subnet_id      = aws_subnet.subnet.id
+  route_table_id = aws_route_table.rt.id
+}
+
+resource "aws_security_group" "ecs_sg" {
+  name        = "ecs_sg"
+  description = "Allow inbound HTTP"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_ecs_cluster" "fargate_cluster" {
+  name = "fargate-cluster"
+}
+
+resource "aws_ecs_task_definition" "flask_task" {
+  family                   = "flask-task"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "256"
+  memory                   = "512"
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+
+  container_definitions = jsonencode([{
+    name      = "flask-app"
+    image     = var.container_image
+    essential = true
+    portMappings = [{
+      containerPort = 5000
+      protocol      = "tcp"
+    }]
+  }])
+}
+
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecsTaskExecutionRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Effect = "Allow",
+      Principal = {
+        Service = "ecs-tasks.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_ecs_service" "flask_service" {
+  name            = "flask-service"
+  cluster         = aws_ecs_cluster.fargate_cluster.id
+  task_definition = aws_ecs_task_definition.flask_task.arn
+  launch_type     = "FARGATE"
+  desired_count   = 1
+
+  network_configuration {
+    subnets          = [aws_subnet.subnet.id]
+    security_groups  = [aws_security_group.ecs_sg.id]
+    assign_public_ip = true
+  }
+}
+
+# variables.tf
+output "cluster_name" {
+  value = aws_ecs_cluster.fargate_cluster.name
+}
+
+output "service_name" {
+  value = aws_ecs_service.flask_service.name
+}
