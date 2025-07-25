e981ca8bc0a420a5d8cce3737d6990763e4a0b25
SK963 <kumarshubham963852741@gmail.com>
2025-07-25 15:40:54 +0530

    code commit

--- app.py
+++ app.py
@@ -8,5 +8,10 @@ app = Flask(__name__)
 def home():
     return "Hello from Flask App : Shubham Kumar"
 
+
+@app.route('/about')
+def about():
+    return "This is the About page of the Flask app built by shubham kumar."
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
